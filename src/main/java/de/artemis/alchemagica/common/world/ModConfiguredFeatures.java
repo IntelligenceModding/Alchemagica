1b86a422e1fe0a07dbca542306e92e77896d6752
Artemis <mattigrego@gmx.de>
2023-05-28 18:40:15 +0200

    - Ancient Petal Clusters will now generate, similar to normal ores

--- src/main/java/de/artemis/alchemagica/common/world/ModConfiguredFeatures.java
+++ src/main/java/de/artemis/alchemagica/common/world/ModConfiguredFeatures.java
@@ -1,8 +1,10 @@
 package de.artemis.alchemagica.common.world;
 
+import com.google.common.base.Suppliers;
 import de.artemis.alchemagica.common.registration.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.block.Blocks;
@@ -12,14 +14,22 @@ import net.minecraft.world.level.levelgen.GeodeLayerSettings;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.GeodeConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 import static de.artemis.alchemagica.common.registration.Registration.CONFIGURED_FEATURES;
 
 public class ModConfiguredFeatures {
+
+    public static final Supplier<List<OreConfiguration.TargetBlockState>> OVERWORLD_ANCIENT_PETAL_CLUSTER = Suppliers.memoize(() -> List.of(
+            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.ANCIENT_PETAL_CLUSTER.get().defaultBlockState())));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> ANCIENT_PETAL_CLUSTER = CONFIGURED_FEATURES.register("ancient_petal_cluster",
+            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_ANCIENT_PETAL_CLUSTER.get(),15))); // pSize is the vein size of this ore
 
     public static final RegistryObject<ConfiguredFeature<?, ?>> ARCANE_CRYSTAL_GEODE = CONFIGURED_FEATURES.register("arcane_crystal_geode",
             () -> new ConfiguredFeature<>(Feature.GEODE,
@@ -33,15 +43,7 @@ public class ModConfiguredFeatures {
                                     ModBlocks.MEDIUM_ARCANE_CRYSTAL_BUD.get().defaultBlockState(),
                                     ModBlocks.LARGE_ARCANE_CRYSTAL_BUD.get().defaultBlockState(),
                                     ModBlocks.ARCANE_CRYSTAL_CLUSTER.get().defaultBlockState()),
-                            BlockTags.FEATURES_CANNOT_REPLACE, BlockTags.GEODE_INVALID_BLOCKS),
-                            new GeodeLayerSettings(1.7D, 2.2D, 3.2D, 4.2D),
-                            new GeodeCrackSettings(0.95D, 2.0D, 2),
-                            0.35D, 0.083D,
-                            true,
-                            UniformInt.of(4, 6),
-                            UniformInt.of(3, 4),
-                            UniformInt.of(1, 2),
-                            -16, 16, 0.05D, 1)));
+                            BlockTags.FEATURES_CANNOT_REPLACE, BlockTags.GEODE_INVALID_BLOCKS), new GeodeLayerSettings(1.7D, 2.2D, 3.2D, 4.2D), new GeodeCrackSettings(0.95D, 2.0D, 2),0.35D, 0.083D,true, UniformInt.of(4, 6), UniformInt.of(3, 4), UniformInt.of(1, 2), -16, 16, 0.05D, 1)));
 
     public static void register() {
     }
--- src/main/java/de/artemis/alchemagica/common/world/ModPlacedFeatures.java
+++ src/main/java/de/artemis/alchemagica/common/world/ModPlacedFeatures.java
@@ -10,11 +10,28 @@ import static de.artemis.alchemagica.common.registration.Registration.PLACED_FEA
 
 public class ModPlacedFeatures {
 
+    public static final RegistryObject<PlacedFeature> ANCIENT_PETAL_CLUSTER_PLACED = PLACED_FEATURES.register("ancient_petal_cluster_placed",
+            () -> new PlacedFeature(ModConfiguredFeatures.ANCIENT_PETAL_CLUSTER.getHolder().get(),
+                    commonOrePlacement(30, // Amount of ore veins per chunk
+                            HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-80), VerticalAnchor.aboveBottom(80)))));
+
     public static final RegistryObject<PlacedFeature> ARCANE_CRYSTAL_GEODE = PLACED_FEATURES.register("arcane_crystal_geode_placed",
             () -> new PlacedFeature(ModConfiguredFeatures.ARCANE_CRYSTAL_GEODE.getHolder().get(), List.of(
                     RarityFilter.onAverageOnceEvery(12), InSquarePlacement.spread(),
                     HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(6), VerticalAnchor.absolute(30)),
                     BiomeFilter.biome())));
+
+    public static List<PlacementModifier> orePlacement(PlacementModifier p_195347_, PlacementModifier p_195348_) {
+        return List.of(p_195347_, InSquarePlacement.spread(), p_195348_, BiomeFilter.biome());
+    }
+
+    public static List<PlacementModifier> commonOrePlacement(int p_195344_, PlacementModifier p_195345_) {
+        return orePlacement(CountPlacement.of(p_195344_), p_195345_);
+    }
+
+    public static List<PlacementModifier> rareOrePlacement(int p_195350_, PlacementModifier p_195351_) {
+        return orePlacement(RarityFilter.onAverageOnceEvery(p_195350_), p_195351_);
+    }
 
     public static void register() {
     }

a5eaf501d4380e9c15eef795ce37158190b02e09
Artemis <mattigrego@gmx.de>
2023-08-04 12:41:36 +0200

    - Added Recipes for all Arcane Tools

--- src/main/java/de/artemis/alchemagica/common/data/RecipesProvider.java
+++ src/main/java/de/artemis/alchemagica/common/data/RecipesProvider.java
@@ -1,8 +1,12 @@
 package de.artemis.alchemagica.common.data;
 
+import de.artemis.alchemagica.common.registration.ModItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,5 +20,10 @@ public class RecipesProvider extends RecipeProvider implements IConditionBuilder
 
     @Override
     protected void buildCraftingRecipes(@NotNull Consumer<FinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shaped(ModItems.ARCANE_AXE.get(), 1).define('A', ModItems.ARCANE_STICK.get()).define('B', ModItems.ARCANE_CRYSTAL_SHARD.get()).define('C', Items.GOLD_INGOT).pattern("BC ").pattern("BA ").pattern(" A ").unlockedBy("has_arcane_stick", has(ModItems.ARCANE_STICK.get())).unlockedBy("has_arcane_crystal_shard", has(ModItems.ARCANE_CRYSTAL_SHARD.get())).unlockedBy("has_gold_ingot", has(Items.GOLD_INGOT)).save(consumer);
+        ShapedRecipeBuilder.shaped(ModItems.ARCANE_PICKAXE.get(), 1).define('A', ModItems.ARCANE_STICK.get()).define('B', ModItems.ARCANE_CRYSTAL_SHARD.get()).define('C', Items.GOLD_INGOT).pattern("BCB").pattern(" A ").pattern(" A ").unlockedBy("has_arcane_stick", has(ModItems.ARCANE_STICK.get())).unlockedBy("has_arcane_crystal_shard", has(ModItems.ARCANE_CRYSTAL_SHARD.get())).unlockedBy("has_gold_ingot", has(Items.GOLD_INGOT)).save(consumer);
+        ShapedRecipeBuilder.shaped(ModItems.ARCANE_HOE.get(), 1).define('A', ModItems.ARCANE_STICK.get()).define('B', ModItems.ARCANE_CRYSTAL_SHARD.get()).define('C', Items.GOLD_INGOT).pattern("BC ").pattern(" A ").pattern(" A ").unlockedBy("has_arcane_stick", has(ModItems.ARCANE_STICK.get())).unlockedBy("has_arcane_crystal_shard", has(ModItems.ARCANE_CRYSTAL_SHARD.get())).unlockedBy("has_gold_ingot", has(Items.GOLD_INGOT)).save(consumer);
+        ShapedRecipeBuilder.shaped(ModItems.ARCANE_SHEARS.get(), 1).define('A', ModItems.ARCANE_CRYSTAL_SHARD.get()).pattern("A ").pattern(" A").unlockedBy("has_arcane_crystal_shard", has(ModItems.ARCANE_CRYSTAL_SHARD.get())).save(consumer);
+
     }
 }
